068efde064a6a986a638ca3118251100db94cdf4
orangeDELL <chengzhi37@gmail.com>
2014-01-07 16:37:39 +0800

    edit

--- src/com/hugo/cxyclubnews/MainActivity.java
+++ src/com/hugo/cxyclubnews/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		
+		//Edit by orange;
 	}
 
 }
